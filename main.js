1fbc70c536fb3e2d78623ac03f93a4293c1c6f5a
naiz19232 <33213878+naiz19232@users.noreply.github.com>
2022-01-31 07:47:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+var can = new fabric.Canvas('can');
+ctx = can.getContext("2d")
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+block_image_object= "";
+
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image,function(Img){
+			block_image_object= Img;
+			block_image_object.scaleToWidth(block_image_width);
+			block_image_object.scaleToHeight(block_image_height);
+			block_image_object.set({
+				top:block_y,
+				left:block_x
+			});
+			can.add(block_image_object);
+		}
+	);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+	if(keyPressed == '82'){
+		
+            new_image('rr.jpg');
+            console.log("r");
+        }
+	if(keyPressed == '71'){
+		new_image('gr.png')
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		// upload yellow ranger
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		// upload pink ranger
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+	// upload blue ranger
+	}
+	
+}
